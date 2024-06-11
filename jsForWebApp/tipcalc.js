b181157ab6973b07d6a15926a63b3cc251bef082
JacobGiboney <jacob.giboney@zealitconsultants.com>
2024-06-11 16:25:22 -0500

    the 2.0 version of the application, new ui design angle and added functionality for GL and weather app is online with api

--- jsForWebApp/tipcalc.js
+++ jsForWebApp/tipcalc.js
@@ -6,7 +6,7 @@
 function calculateTip() {
     // Check if the inputs are valid numbers
     var billAmount = parseFloat(document.getElementById("totalBill").value)
-    var tipPercentage = parseFloat(document.getElementById("tipPer").value)
+    var tipPercentage = parseFloat(document.getElementById("myRange").value)
 
     if (isNaN(billAmount) || isNaN(tipPercentage)) 
     {
@@ -21,4 +21,25 @@ function calculateTip() {
 }
 //console.log(calculateTip(billTot, tipPer));
 console.log('hello');
+
+// document.getElementById("totalBill").addEventListener("keypress", function(event) {
+//     if (event.key === "Enter") {
+//         calculateTip();
+//     }
+// });
+
+var slider = document.getElementById("myRange");
+
+// Function to handle slider change event
+function handleSliderChange(event) {
+    // Get the current value of the slider
+    var sliderValue = event.target.value;
+
+    // Do something with the slider value, for example update a display
+    calculateTip()
+}
+
+// Add event listener to the slider
+slider.addEventListener("input", handleSliderChange);
+
 
